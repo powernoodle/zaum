a2d05ab2af9e086c20904983c1965bd5ea0f7eb1
broth <bob.roth@powernoodle.com>
2019-11-04 15:58:47 -0500

    Now next.jdbc free

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject org.zaumdb/zaum "0.2.7"
+(defproject org.zaumdb/zaum "0.2.8"
   :description "FIXME: write description"
   :url "https://github.com/dkropfuntucht/zaum"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure       "1.9.0-RC1"]
-                 [org.clojure/clojurescript   "1.9.946"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [seancorfield/next.jdbc "1.0.1"]])
+                 [org.clojure/clojurescript   "1.9.946"]])
